727ab3f0933cb4e2c8b4a0d89def7524734d74d1
chenyg <m15877032720@163.com>
2021-10-28 18:03:26 +0800

    添加 合并有序链表 练习

---
+++ thinking-in-algorithm/sort/sort-list/sort.go
@@ -0,0 +1,17 @@
+package main
+
+// ListNode 链表
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// findMiddleNode 查找中间结点
+func findMiddleNode(head *ListNode) *ListNode {
+
+}
+
+// sortList 使用合并排序
+func sortList(head *ListNode) *ListNode {
+
+}
